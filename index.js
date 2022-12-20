1cacef092a9e28ba5d173cd345f4887fe6a4d6ba
JordanR-img <jordanrobin18@gmail.com>
2022-12-20 17:41:21 +1300

    mission4

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-const CalculatePremium = require('./PremiumCalculator')
-const express = require("express")
-const http = require('http')
+const CalculatePremium = require("./PremiumCalculator");
+const express = require("express");
+const http = require("http");
 
+const server = express();
+const env = require("dotenv");
+env.config();
+server.use(express.json());
 
-
-
-const server = express()
-const env = require('dotenv')
-env.config()
-server.use(express.json())
-
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => {
-    console.log("i'm listening on port", PORT)
-})
+  console.log("i'm listening on port", PORT);
+});
 
 // let cars = {"Make": "toyota", "Make": "honda"}
 
 // console.log(cars.Make)
 
-
 // function testPremium(car) {
 //     return {'cool': car.car_value * car.risk_rating / 100};
 // }
 
-
 server.post("/PremiumQuote", (req, res) => {
+  let carValue = req.body.carValue;
+  let riskRating = req.body.riskRating;
 
-    let carValue = req.body.carValue
-    let riskRating = req.body.riskRating
+  // console.log(carValue)
+  // console.log(riskRating)
+  const quote = CalculatePremium(carValue, riskRating);
 
-    // console.log(carValue)
-    // console.log(riskRating)
-     const quote = CalculatePremium(carValue, riskRating)
-
-        res.send(quote)
-    // res.send(quote)
-    // res.sendStatus(quote)
-})
+  res.send(quote);
+  // res.send(quote)
+  // res.sendStatus(quote)
+});
 
 // res.send(quote + "") // In case the send breaks, try this
-
 
 // let testCar = {"car_value": 6614, "risk_rating": 5}
 // testPremium(testCar);
@@ -57,16 +50,11 @@ server.post("/PremiumQuote", (req, res) => {
 //     res.sendStatus(45);
 // })
 
-
 // function testPremium2() {
 //     return testCar2.car_value * testCar2.risk_rating / 100
 // }
-
 
 // console.log(testPremium2())
 
 module.exports = server;
 // module.exports = testPremium()
-
-
-
